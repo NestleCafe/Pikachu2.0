39ddddeed21a380e60f18da79c5d8ddb36b5ac2d
NestleCafe <watson_xian@foxmail.com>
2021-05-25 01:43:00 +0800

    修复不能使用import的问题

---
+++ dist/string.ff0d7fd2.js
@@ -0,0 +1,334 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"string.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var string = ".skin *{box-sizing: border-box; margin:0; padding:0;}\n.skin *::before, *::after{box-sizing: border-box;}\nbody{\n    height: 100vh;\n    background: rgb(255,230,0);\n}\n.skin{\n    position: relative;\n    min-height: 50vh;\n    position: relative;\n}\n\n@keyframes wave{\n    0%{\n        transform: rotate(0deg);\n    }\n    33%{\n        transform: rotate(0.5deg);\n    }\n    66%{\n        transform: rotate(-0.5deg);\n    }\n    100%{\n        transform: rotate(0deg);\n    }\n}\n.nose:hover{\n    transform-origin: center bottom;\n    animation: wave 0.23s infinite linear;\n}\n.nose-bottom{\n    position: relative;\n    height: 0px;\n    width: 0px;\n    border: 10px solid black;\n    border-bottom: none;\n    border-color: black transparent transparent;\n    left: 50%;\n    top: 145px;\n    margin-left: -10px;\n}\n\n.nose-top{\n    position: absolute;\n    height: 6px;\n    width: 19px;\n    top: -13px;\n    left: -9.5px;\n    border-color: black  black  transparent black;\n    border-radius: 50% ;\n    background: black;\n}\n.eyebrow{\n    position: absolute;\n    border: 0.3em solid black;\n    height: 30px;\n    width: 100px;\n    left: 50%;\n    margin-left: -50px;\n    top: 125px;\n    display: none;\n}\n.eyebrow.left{\n    border-color: transparent black  black transparent;\n    border-radius: 0 0 80% 0;\n    transform: rotate(32deg) translateX(-100px);\n}\n.eyebrow.right{\n    border-color: transparent  transparent  black black;\n    border-radius: 0 0 0 80%;\n    transform: rotate(-32deg) translateX(100px);\n}\n\n\n.eye{\n    border: 3px solid black;\n    width: 64px;\n    height: 64px;\n    border-radius: 50%;\n    background: rgb(46, 46, 46);\n    position: absolute;\n    left: 50%;\n    margin-left: -32px;\n    top: 100px;\n}\n.eye::before{\n    content: '';\n    display: block;\n    border: 3px solid black;\n    width: 32px;\n    height: 32px;\n    background: white;\n    border-radius: 50%;\n    position: relative;\n    left: 8px;\n}\n.eye.left{\n    border: 1px solid black;\n    transform: translateX(-100px);\n}\n.eye.right{\n    transform: translateX(100px);\n}\n.mouth{\n    width: 200px;\n    height: 200px;\n    position: absolute;\n    left: 50%;\n    top: 170px;\n    margin-left: -100px;\n}\n\n.mouth .top {\n    position: relative;\n    z-index: 1;\n}\n\n.mouth .top .lip{\n    position: absolute;\n    border: 0.3em solid black;\n    height: 30px;\n    width: 100px;\n    background: rgb(255,230,0);\n}\n.mouth .top .lip::before{ \n    content: '';\n    display: block;\n    height: 13px;\n    margin-top: -13px;\n    background: rgb(255,230,0);\n}\n\n.mouth .top .lip.left{\n    border-color: transparent  transparent  black black;\n    border-radius: 0 0 0 80%;\n    transform: rotate(-32deg);\n    left: 4px;\n}\n\n.mouth .top .lip.right{\n    border-color: transparent black  black transparent;\n    border-radius: 0 0 80% 0;\n    transform: rotate(32deg);\n    left: calc(50% - 4px);\n}\n.mouth .bottom{\n    position: absolute;\n    height: 180px;\n    width: 100%;\n    overflow: hidden;\n    top: 3px;\n}\n.mouth .bottom .tongue{\n    border: 3px solid black;\n    width: 150px;\n    position: absolute;\n    height: 700px;\n    bottom: 0;\n    left: 50%;\n    margin-left: -75px;\n    border-radius: 0 0 60% 60%;\n    background: rgb(155,0,10);\n    overflow: hidden;\n}\n.mouth .bottom .tongue .jaw{\n    background: rgb(255,72,95);\n    width: 200px;\n    height: 300px;\n    position: absolute;\n    bottom: -160px;\n    left: 50%;\n    margin-left: -100px;\n    border-radius: 50%;\n}\n\n.face{\n    position: absolute;\n    left: 50%;\n    border: 3px solid black;\n    border-radius: 50%;\n    width: 88px;\n    height: 88px;\n    top: 220px;\n    margin-left: -44px;\n    background: red;\n}\n.face.left{\n    transform: translateX(-170px);\n}\n.face.right{\n    transform: translateX(170px);\n}\n.face > img{\n    position: absolute;\n    top: 50%;\n    left: 50%;\n    display: none;\n}\n.face.left > img{\n    transform: rotateY(180deg);\n    transform-origin: 0 0;\n}\n\n@keyframes blur {\n    0%{\n      text-shadow:0px 0px 10px #fff;\n    }\n    20%{\n      text-shadow:0px 0px 25px #fff; \n    }\n    40%{\n      text-shadow:0px 0px 50px #fff;\n    }\n    60%{\n      text-shadow:0px 0px 50px #7B96B8;\n    }\n    80%{\n      text-shadow:0px 0px 150px #7B96B8;\n    }\n    100%{\n      text-shadow:0px -10px 100px #7B96B8,\n    }\n  }\n.caution {\n    display: none;\n    position: absolute;\n    bottom: 0;\n    left: 50%;\n    margin-left: -330px;\n    font-size: 6.5em;\n    color: #fff;\n    animation: blur 1s  infinite;\n    text-shadow:0px 0px 7px #fff;\n    z-index: 5;\n    pointer-events: none;\n  }\n\n";
+var _default = string;
+exports.default = _default;
+},{}],"C:/Users/Nestle/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55346" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else {
+        window.location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["C:/Users/Nestle/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","string.js"], null)
+//# sourceMappingURL=/string.ff0d7fd2.js.map
